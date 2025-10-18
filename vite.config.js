d070e8e8146a9531375bbe6e95dd010486b57dee
ramtarun02 <ta.ramtarun.19@gmail.com>
2025-10-18 03:35:28 +0100

    commit

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-    base: '/VFP-2025',
+    base: 'VFP-2025',
     define: {
         'process.env.NODE_ENV': JSON.stringify('production'),
         global: 'globalThis',
