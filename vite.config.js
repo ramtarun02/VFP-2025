16e722577b413795bdfacd11d1fa63b99465faa5
ramtarun02 <ta.ramtarun.19@gmail.com>
2025-10-18 03:27:09 +0100

    gh-page Deploy

--- vite.config.js
+++ vite.config.js
@@ -2,16 +2,20 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+    base: '/VFP-2025',
+    define: {
+        'process.env.NODE_ENV': JSON.stringify('production'),
+        global: 'globalThis',
+    },
+
     plugins: [react()],
     server: {
         port: 3000,
         open: true
     },
     build: {
-        outDir: 'build'
-    },
-    define: {
-        global: 'globalThis',
+        outDir: 'dist',
+        assetsDir: 'assets',
     },
     css: {
         postcss: './postcss.config.js'
